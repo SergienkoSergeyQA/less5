5a86ba52c549a2d89289ff5f2134d5fa3ebf246a
Sergey Sergienko <sergienckoss@gmail.com>
2021-05-15 09:31:25 +0300

    commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+
     }
 }
